b920ed3afb947d77e21f0fc834120e11fa72fc41
Herigony <40726633+Herigony@users.noreply.github.com>
2019-07-10 15:34:12 -0500

    Allow deconstruction of NonDestroyables
    for deconstructing geysers

---
+++ Source/MapEditTools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace MapEditTools
+{
+    [StaticConstructorOnStartup]
+    public static class MapEditTools
+    {
+        static MapEditTools()
+        {
+            Thing.allowDestroyNonDestroyable = true;
+        }
+    }
+}
